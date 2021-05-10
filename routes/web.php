7f15caf06b95c4b7d2294742dd456ad1bc67d7e0
Nabilah Argyanti <nargyanti@gmail.com>
2021-05-10 20:39:53 +0700

    update composer

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,7 @@ Route::get('/setting-reset', function () {
 Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
