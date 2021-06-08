3a272df9e872108622a39a2737dba54285f7f904
AikaMinami <aikatan.aikachan@gmail.com>
2021-06-08 16:52:08 +0700

    Change Navbar for forgot password

--- resources/views/newPassword.blade.php
+++ resources/views/newPassword.blade.php
@@ -1,18 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.indexLayout')
 
 @section('content')
-    <div class="container">
-        <div class="vertical-center">
-            <div class="container border " style="padding:75px"> 
-                <form action="{{ route('new_password', $user->id) }}">
-                    @csrf
-                    <label for="new-pass">Input Your New Password</label>
-                    <input class="form-control" type="password" placeholder="New Password" name="password" id="new-pass"><br>                    
-                    <label for="pass-confirm">Re-Input Your New Password</label>
-                    <input class= "form-control" type="password" placeholder="Confirm New Password" name="confirm_password" id="pass-confirm"><br>
-                    <button type="submit" class="btn btn-primary float-right" style="">Confirm</button>
-                </form>
-            </div>            
+    <div class="container" style="margin-top:7.5%">
+        <div class="container border" style="padding:75px"> 
+            <form action="{{ route('new_password', $user->id) }}">
+                @csrf
+                <label for="new-pass">Input Your New Password</label>
+                <input class="form-control" type="password" placeholder="New Password" name="password" id="new-pass"><br>                    
+                <label for="pass-confirm">Re-Input Your New Password</label>
+                <input class= "form-control" type="password" placeholder="Confirm New Password" name="confirm_password" id="pass-confirm"><br>
+                <button type="submit" class="btn btn-primary float-right" style="">Confirm</button>
+            </form>        
         </div>
     </div>
 @endsection
